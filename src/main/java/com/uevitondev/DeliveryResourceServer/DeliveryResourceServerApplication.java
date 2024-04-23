201a3aca4ff59474efeff7987e40d5b52525be5a
uevitondev <uevitonsantos@outlook.com>
2024-04-23 14:39:37 -0300

    project created

---
+++ src/main/java/com/uevitondev/DeliveryResourceServer/DeliveryResourceServerApplication.java
@@ -0,0 +1,13 @@
+package com.uevitondev.DeliveryResourceServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryResourceServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryResourceServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uevitondev/DeliveryResourceServer/DeliveryResourceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uevitondev.DeliveryResourceServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryResourceServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
